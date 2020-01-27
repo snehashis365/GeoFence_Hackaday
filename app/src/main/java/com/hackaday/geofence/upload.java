c26b7ffa8c926068f92ad2affb3050aaf9838935
snehashis365 <snehashis.2000@gmail.com>
2020-01-27 17:15:28 +0530

    Added camera intent Added a image view to preview captured image Added permission check

--- app/src/main/java/com/hackaday/geofence/upload.java
+++ app/src/main/java/com/hackaday/geofence/upload.java
@@ -1,14 +1,104 @@
 package com.hackaday.geofence;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.HapticFeedbackConstants;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class upload extends AppCompatActivity {
 
+    ImageButton camera,browse;
+
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    ImageView preview;
+
+    Uri image_uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        camera=findViewById(R.id.btn_camera);
+        browse=findViewById(R.id.btn_browse);
+        preview=findViewById(R.id.preview);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED) {
+                        //permission not enabled, request it
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //show popup to request permissions
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {
+                        openCamera();
+
+                    }
+                } else {
+
+                    openCamera();
+                }
+            }
+
+        });
+    }
+
+        private void openCamera() {
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "GeoFence Image");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+            image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            //Camera intent
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+            startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+        }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //this method is called, when user presses Allow or Deny from Permission Request Popup
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera();
+                }
+                else {
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image was captured from camera
+        super.onActivityResult(requestCode,resultCode,data);
+        if (resultCode == RESULT_OK){
+            //set the image captured to our ImageView
+            preview.setImageURI(image_uri);
+        }
     }
 }
